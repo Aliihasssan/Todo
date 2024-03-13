bce74e86ef8cbefd5926684dc9a6bf7e383c4d54
Ali hassan <aliwally211@gmail.com>
2024-03-13 19:19:56 +0200

    add java code

--- js/code.js
+++ js/code.js
@@ -1,1 +1,11 @@
 console.log("Hello javasript")
+
+const todoBtn = document.querySelector(".add-btn")
+const todoInput = document.querySelector(".form-control")
+
+function addTodo(e){
+    e.preventDefault();
+console.log(todoInput.value)
+ todoInput.value=""
+}
+todoBtn.addEventListener('click',addTodo)
